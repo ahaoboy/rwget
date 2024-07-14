9abb4d3c989fee2c27f773b60768ed6c15199f8e
ahaoboy <504595380@qq.com>
2024-07-14 17:21:04 +0800

    :support napi

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
+use clap::Parser;
 use reqwest::Client;
 use std::fs::File;
 use std::io::copy;
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long)]
+    pub out_file: Option<String>,
+
+    #[clap()]
+    pub url: String,
+}
 
 pub async fn download_file(url: &str, out_file: &str) -> Result<(), Box<dyn std::error::Error>> {
     // Create a client
@@ -22,9 +34,15 @@ pub async fn download_file(url: &str, out_file: &str) -> Result<(), Box<dyn std:
         // Copy the response content to the file
         let content = response.bytes().await?;
         copy(&mut content.as_ref(), &mut dest)?;
-        println!("File downloaded successfully to {}", out_file);
-    } else {
-        eprintln!("Failed to download file: {}", response.status());
     }
     Ok(())
+}
+
+pub async fn rwget(args: Args) {
+    let url = args.url;
+    let path = Path::new(&url);
+    let file_name = args
+        .out_file
+        .unwrap_or(path.file_name().unwrap().to_str().unwrap().to_string());
+    download_file(&url, &file_name).await.unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-use std::path::Path;
-
 use clap::Parser;
-use rwget::download_file;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub(crate) struct Cli {
-    #[arg(short, long)]
-    out_file: Option<String>,
-
-    #[clap()]
-    url: String,
-}
+use rwget::{rwget, Args};
 
 #[tokio::main]
 async fn main() {
-    let cli = Cli::parse();
-
-    let url = cli.url;
-    let path = Path::new(&url);
-    let file_name = cli
-        .out_file
-        .unwrap_or(path.file_name().unwrap().to_str().unwrap().to_string());
-    download_file(&url, &file_name).await.unwrap();
+    let args = Args::parse();
+    rwget(args).await;
 }
